3c99f90636e3ac50f611db437871cf07cf3d0d5f
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-01-31 20:59:36 +0900

    style/#6 : HeaderWrapper 스타일링

--- src/commons/Header.tsx
+++ src/commons/Header.tsx
@@ -1,13 +1,31 @@
-import React from "react";
 import InfoIc from "../assets/info.svg";
+import styled from "@emotion/styled";
 
 const Header = () => {
   return (
-    <div>
-      <img src={InfoIc} />
-      헤더임
-    </div>
+    <HeaderWrapper>
+      GroundGuardians
+      <InfoIcon src={InfoIc} />
+    </HeaderWrapper>
   );
 };
 
 export default Header;
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  justify-content: center;
+  width: 100vw;
+  height: 60px;
+  box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.1);
+  color: #25c35c;
+  font-size: 16px;
+  font-weight: 900; //적용 안되는거처럼 보임
+`;
+
+const InfoIcon = styled.img`
+  position: relative;
+  left: 30px;
+`;
