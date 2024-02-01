af1ceca47a4a8217caff6983cb8d1ed056a9cae9
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-02-01 18:53:55 +0900

    feat/#14 : layout 구조 잡기

--- src/commons/Layout.tsx
+++ src/commons/Layout.tsx
@@ -1,10 +1,15 @@
-import React from "react";
 import styled from "@emotion/styled";
+import { CommonButton } from "./Button";
 
 const Layout = () => {
   return (
     <LayoutWrapper>
-      <Question>질문</Question>
+      <QuestionText>질문</QuestionText>
+      <QuestionImage>이미지</QuestionImage>
+      <CommonButton isGreen={true}>
+        I wake up on a bright sunny day.
+      </CommonButton>
+      <CommonButton isGreen={false}>I wake up on a moonlit night.</CommonButton>
     </LayoutWrapper>
   );
 };
@@ -13,10 +18,19 @@ export default Layout;
 
 const LayoutWrapper = styled.div`
   display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
-const Question = styled.div`
+const QuestionText = styled.div`
   width: 390px;
   height: 102px;
   border: 1px solid black;
+`;
+
+const QuestionImage = styled.div`
+  width: 390px;
+  height: 200px;
+  border: 1px solid yellow;
+  margin-bottom: 27px;
 `;
