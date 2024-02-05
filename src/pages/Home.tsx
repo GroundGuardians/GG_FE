9e878ff3f9dfea5186b8db29892dd9322bdc89ac
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-02-05 22:19:04 +0900

    style/#19 : Title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,26 @@
+import styled from "@emotion/styled";
+
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <Title>
+      "If I were to turn
+      <br /> into an animal?"
+    </Title>
+  );
 };
 
 export default Home;
+
+const Title = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 248px;
+  height: 100px;
+  margin-top: 60px;
+
+  color: #20a74f;
+  font-size: 36px;
+  text-align: center;
+  -webkit-text-stroke-width: 1px;
+  -webkit-text-stroke-color: #20a74f;
+`;
