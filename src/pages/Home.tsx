5172d25f7d73fb3951ad159d9d540f1e8ce8b6d4
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-02-05 22:26:46 +0900

    style/#19 : 필요한 svg 다운 및 스타일링

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,17 @@
 import styled from "@emotion/styled";
+import HomeImg from "../assets/home.svg";
+import GoogleLogin from "../assets/GoogleLogin.svg";
 
 const Home = () => {
   return (
-    <Title>
-      "If I were to turn
-      <br /> into an animal?"
-    </Title>
+    <>
+      <Title>
+        "If I were to turn
+        <br /> into an animal?"
+      </Title>
+      <img src={HomeImg} />
+      <GoogleLoginIcon src={GoogleLogin} />
+    </>
   );
 };
 
@@ -21,6 +27,12 @@ const Title = styled.div`
   color: #20a74f;
   font-size: 36px;
   text-align: center;
-  -webkit-text-stroke-width: 1px;
+  -webkit-text-stroke-width: 1pt;
   -webkit-text-stroke-color: #20a74f;
+`;
+
+const GoogleLoginIcon = styled.img`
+  margin-top: 39px;
+
+  cursor: pointer;
 `;
