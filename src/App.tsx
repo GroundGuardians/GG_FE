06501bce0b3f49a29d4e0f55c813d711759689a1
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-02-05 23:10:40 +0900

    feat/#8 : 캐러셀 기능 동작

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import Header from "./commons/Header";
-import Layout from "./commons/Layout";
 import styled from "@emotion/styled";
-import Question1 from "../src/assets/question1.svg";
+import EmblaCarousel from "./Carousel";
+import { EmblaOptionsType } from "embla-carousel";
+import "./embla.css";
+
+const SLIDES = Array.from(Array(3).keys());
+const OPTIONS: EmblaOptionsType = {};
 
 function App() {
   return (
     <Wrapper>
       <Header />
-      <Layout
-        questionText="Q1. I woke up and I have transformed 
-into an animal."
-        questionImage={Question1}
-        buttonGreenText="I wake up on a bright sunny day."
-        buttonWhiteText="I wake up on a moonlit night."
-      />
+      <section className="sandbox__carousel">
+        <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+      </section>
     </Wrapper>
   );
 }
--- src/imageByIndex.ts
+++ src/imageByIndex.ts
@@ -1,7 +1,7 @@
-import image1 from "./assets/info.svg";
-import image2 from "./assets/info.svg";
-import image3 from "./assets/info.svg";
-import image4 from "./assets/info.svg";
+import image1 from "./assets/question1.svg";
+import image2 from "./assets/question2.svg";
+import image3 from "./assets/question3.svg";
+import image4 from "./assets/question4.svg";
 
 export const images: string[] = [image1, image2, image3, image4];
 
