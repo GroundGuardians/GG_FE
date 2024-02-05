3ac7b4ba4fea5fe345bc43b10fe9d01bf416078a
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-02-05 20:31:24 +0900

    feat/#16 : 라우팅 처리

---
+++ src/Router.tsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import MyPage from "./pages/MyPage";
+import Quiz from "./pages/Quiz";
+import Survey from "./pages/Survey";
+import App from "./App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "/mypage",
+        element: <MyPage />,
+      },
+      {
+        path: "/survey",
+        element: <Survey />,
+      },
+      {
+        path: "/quiz",
+        element: <Quiz />,
+      },
+    ],
+  },
+]);
+
+export default router;
