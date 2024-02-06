16f4a788e52151d4abdb8be132a268767c379222
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-02-06 16:07:42 +0900

    style/#22 : 퀴즈 뷰 스타일링 상세 조정

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -21,51 +21,64 @@ export default Quiz;
 
 const Title = styled.div`
   margin-top: 20px;
+
   font-size: 40px;
   -webkit-text-stroke-width: 0.5pt;
   -webkit-text-stroke-color: #000;
 `;
 
 const QuizQuestion = styled.div`
-  margin-top: 20px;
   display: flex;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
   width: 392px;
   height: 239px;
+  margin-top: 20px;
+
   background: #eaf7ef;
 `;
 
 const QuizAnswerWrapper = styled.div`
   display: flex;
-  justify-content: space-around;
   align-items: center;
-  text-align: center;
+  justify-content: space-between;
   width: 230px;
   height: 37px;
-  border: 1px solid yellow;
   margin-top: 20px;
+
+  text-align: center;
+
+  border: 1px solid yellow;
 `;
 
 const QuizAnswerMark = styled.div`
-  font-size: 32px;
-  border: 1px solid black;
   width: 21px;
   height: 37px;
+
+  font-size: 32px;
   line-height: 32px;
 `;
 
 const QuizAnswerInput = styled.input`
   width: 201px;
+  height: 27px;
+  margin-top: 3px;
+  padding: 0;
+
+  font-size: 24px;
+  font-family: GangwonEduSaeeum;
+
+  border: 0;
 
   ::placeholder {
     color: #898989;
     font-size: 24px;
-    font-family: "GangwonEduSaeeum";
+    font-family: GangwonEduSaeeum;
   }
 `;
 
 const QuizAnswerInputBorder = styled.div`
   height: 37px;
+
   border-bottom: 2px solid #c9c9c9;
 `;
