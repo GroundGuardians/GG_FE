a375a137d4981432af372c7262f2a3976dfd73c0
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-02-18 20:26:56 +0900

    feat/#38 : fetch mypage 정보

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -3,6 +3,9 @@ import { CommonButton, ButtonRowWrapper } from "../commons/Button";
 import { useNavigate } from "react-router-dom";
 import MyPageCarousel from "./mypageCarousel/mypageCarousel";
 import { EmblaOptionsType } from "embla-carousel";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
 
 import "./mypageCarousel/embla.css";
 import "./mypageCarousel/sandbox.css";
@@ -12,11 +15,46 @@ const SLIDE_COUNT = 5;
 const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 
 const MyPage = () => {
+  interface myDataPropType {
+    name: string;
+    email: string;
+    cardImage: Array<ArrayBuffer>;
+  }
   const navigate = useNavigate();
+  const token = localStorage.getItem("jwtToken");
+  const [myData, setMyData] = useState<myDataPropType>();
+
+  useEffect(() => {
+    const handleFetchQuizData = async () => {
+      try {
+        const response = await axios.get(
+          `http://3.38.77.109:8081/mypage`,
+
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        console.log(response, "res");
+
+        console.log(token, "token");
+        setMyData(response.data);
+
+        return response.data;
+      } catch (error) {
+        console.log(token, "token");
+        console.log("에러:", error);
+        throw error;
+      }
+    };
+    handleFetchQuizData();
+  }, []);
+
   return (
     <>
-      <Username>Hello! Eunbi Hwang</Username>
-      <Email>pintotoro123@ajou.ac.kr</Email>
+      <Username>Hello! {myData?.name}</Username>
+      <Email>{myData?.email}</Email>
       <main className="sandbox">
         <section className="sandbox__carousel mypage">
           <MyPageCarousel slides={SLIDES} options={OPTIONS} />
